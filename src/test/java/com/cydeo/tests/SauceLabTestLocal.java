9432eea32970a97d22cbc322e506ad762251db47
oscar <oscar@cybertekschool.com>
2023-09-30 11:43:52 -0400

    SauceLab native app test from local computer

--- src/test/java/com/cydeo/tests/SauceLabTestLocal.java
+++ src/test/java/com/cydeo/tests/SauceLabTestLocal.java
@@ -2,6 +2,7 @@ package com.cydeo.tests;
 
 import com.cydeo.utils.Driver;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,13 @@ public class SauceLabTestLocal {
         AppiumDriver driver = Driver.getDriver();
 
         System.out.println(driver.getDeviceTime());
+
+        // userName Box element and send input
+        driver.findElement(MobileBy.AccessibilityId("test-Username")).sendKeys("standard_user");
+        // password
+        driver.findElement(MobileBy.AccessibilityId("test-Password")).sendKeys("secret_sauce");
+        // submit
+        driver.findElement(MobileBy.AccessibilityId("test-LOGIN")).click();
 
         Driver.closeDriver();
     }
